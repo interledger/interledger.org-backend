60102d3a404302d8698e4f9b817d99bad3d6a5a6
Marcus Williams <marcus.williams@joipolloi.com>
2023-07-31 17:41:47 +0100

    Fix next config

--- config/development/development.config.php
+++ config/development/development.config.php
@@ -1,9 +1,9 @@
 <?php
 $base_url = $_SERVER['BASE_URL'];
 // nextjs
-$config['next.next_site.tuos_dev']['base_url'] = $base_url;
-$config['next.next_site.tuos_dev']['preview_url'] = $base_url . '/api/preview';
-$config['next.next_site.tuos_dev']['revalidate_url'] = $base_url . '/api/revalidate';
+$config['next.next_site.client']['base_url'] = $base_url;
+$config['next.next_site.client']['preview_url'] = $base_url . '/api/preview';
+$config['next.next_site.client']['revalidate_url'] = $base_url . '/api/revalidate';
 
 // graphql
 $config['graphql.graphql_servers.test']['caching'] = true;
